fc452243ab5a8eddb745c7aef36c61873cb7deb2
PamelaNahum <pamela.nahum14@gmail.com>
2023-02-16 12:11:11 -0300

    first commit

---
+++ src/main/java/com/generation/clase1602/Clase1602Application.java
@@ -0,0 +1,12 @@
+package com.generation.clase1602;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase1602Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/test/java/com/generation/clase1602/Clase1602ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.clase1602;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase1602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

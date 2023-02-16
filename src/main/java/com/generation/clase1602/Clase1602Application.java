fcf8fbc5b6c8d3e542b4bee4d37b08d8b444d1c8
PamelaNahum <pamela.nahum14@gmail.com>
2023-02-16 12:13:23 -0300

    agrego comentario

--- src/main/java/com/generation/clase1602/Clase1602Application.java
+++ src/main/java/com/generation/clase1602/Clase1602Application.java
@@ -5,7 +5,42 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Clase1602Application {
 
+	/* Condicionales en JS
+	 * if(condicion){
+	 * se ejecuta aquí si se cumple la condicion
+	 * }else if(condicion) {
+	 * si no se cumple la de arriba pero la de aquí, entra aquí
+	 * }else{
+	 * si no cumplen ninguna de las condiciones anteriores, se realiza esta opción
+	 * }
+	 * Esto se escribe y funciona exactamente igual en JAVA 
+	 */
+
+	 /*Cuales son los ciclos que conocemos
+		For, while, do...while
+	  * for en JS
+		for(let i = 0; condición; i++){
+			mientras la condición sea verdad, esto se va a ejecutar :D
+		}
+		while en JS
+		while(condición){
+			mientras la condición sea verdad, esto se va a ejecutar :D
+		}
+
+		do{
+			entra al menos una vez y luego comprueba la condición y si es verdadera vuelve a iterar :D
+		}while(condición);
+		-------------------------------
+		los while se ecriben exactamente igual en JV, pero el for cambia LEVEMENTE
+
+		for(int i = 0; condición; i++){
+			mientras la condición sea verdad, esto se va a ejecutar :D
+		}
+
+	  */
+
 	public static void main(String[] args) {
+		
 		
 	}
 
